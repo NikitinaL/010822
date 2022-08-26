cf231b6ce810afe61057c8d23f51c0839de6ba80
NikitinaL <LNikitina@appline.ru>
2022-08-26 17:35:07 +0300

    commit

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -2,22 +2,36 @@ import java.util.Random;
 
 public class Task3 {
     public static void main(String[] args) {
-        int[] arr = new int[20];
+        Integer [] arr = new Integer[20];
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = ((int) (Math.random() * 20) - 10);
             System.out.println(arr[i]);
         }
-        int Max = 0;
-        int Min = 0;
+        getMaxMin(arr);
+    }
+
+    private static void getMaxMin(Integer[] arr) {
+        int minIndex = 0;
+        int maxIndex = 0;
+
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[Max]) {
-                Max = i;
-            } else if (arr[i] < arr[Min]) {
-                Min = i;
+            if (arr[i - 1] < arr[i] && arr[i - 1] >= 0) {
+                maxIndex = i - 1;
+            }
+            if (arr[i - 1] > arr[i] && arr[i - 1] < 0) {
+                minIndex = i - 1;
             }
         }
-        System.out.println("Максимум: " + arr[Max] + "\nМинимум: " + arr[Min]);
+
+        int max = arr[maxIndex];
+        int min = arr[minIndex];
+        arr[maxIndex] = min;
+        arr[minIndex] = max;
+        System.out.println("Максимум: " + min + "\nМинимум: " + max);
     }
 }
+
+
+
 
